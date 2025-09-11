5b93a23ebe7bbf9c0ce840af13c15ae2bce52f38
jongseunghan@naver.com <jongseunghan@naver.com>
2025-09-11 16:37:03 +0900

    [Feat] 비동기로 이메일 발송
    * Swagger 정보 변경

--- src/main/java/com/behcm/domain/auth/service/EmailVerificationService.java
+++ src/main/java/com/behcm/domain/auth/service/EmailVerificationService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -78,7 +79,8 @@ public class EmailVerificationService {
         log.info("Email verification completed for: {}", email);
     }
 
-    private void sendEmail(String to, String verificationCode) {
+    @Async
+    public void sendEmail(String to, String verificationCode) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(to);

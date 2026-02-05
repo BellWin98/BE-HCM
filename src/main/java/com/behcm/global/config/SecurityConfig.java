25e0e868de18876fe8e80e4982a85376d418187f
“BellWin98” <jongseunghan@naver.com>
2026-02-06 01:22:34 +0900

    cors 추가

--- src/main/java/com/behcm/global/config/SecurityConfig.java
+++ src/main/java/com/behcm/global/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "https://localhost:*", "https://hcm-red.vercel.app"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "https://localhost:*", "https://hcm-red.vercel.app", "https://hcm-blue.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 

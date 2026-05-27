5375c24f53b13668f73ad59503fca3810bc7b04b
jongseunghan@naver.com <jongseunghan@naver.com>
2026-05-27 16:41:06 +0900

    Transactional read only 제거

--- src/main/java/com/behcm/domain/notification/service/NotificationFacade.java
+++ src/main/java/com/behcm/domain/notification/service/NotificationFacade.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class NotificationFacade {
 
     private final WorkoutRoomRepository workoutRoomRepository;
